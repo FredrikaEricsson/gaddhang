b7a4de8f885270f08d13d88dd54a1f7c9bfe1e35
fredrikaericsson <fredrika@finnvid.se>
2021-04-19 15:10:32 +0200

    created small animation

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = (event) => {
+  let pikeBus = document.getElementById("pikeBus");
+  gsap.set(pikeBus, {
+    rotation: 10,
+  });
+  gsap.to(pikeBus, {
+    rotation: -10,
+    duration: 2,
+    repeat: -1,
+    repeatDelay: 3,
+  });
+  gsap.to(pikeBus, {
+    rotation: 10,
+    duration: 3,
+    delay: 2,
+    repeat: -1,
+    repeatDelay: 2,
+  });
+
+  gsap.to(pikeBus, {
+    x: 160,
+    duration: 5,
+    repeat: -1,
+    ease: "none",
+    yoyo: true,
+  });
+};
